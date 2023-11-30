3bfcac4240f4d6761dcdef1dc6916adc0e86f254
Salmamjg <salmamejgarii2019@gmail.com>
2023-11-30 16:57:29 +0100

    Updated

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,8 +3,8 @@
 if __name__ == "__main__":
     import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
+    list = dir(hidden_4)
+    for name in list:
         if name[:2] != "__":
             print(name)
      
