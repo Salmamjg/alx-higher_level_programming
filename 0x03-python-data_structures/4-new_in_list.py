ba257ad2663dbc43e6a5807535df60c7b5492085
Salmamjg <salmamejgarii2019@gmail.com>
2023-12-04 12:19:07 +0100

    Updated

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list.copy()
+    second_list = my_list.copy()
+    second_list[idx] = element
+
+    return second_list
+ 
