4699d865cadca179ac8f3b6b3a092a66e24e1e4c
Salmamjg <salmamejgarii2019@gmail.com>
2023-12-04 12:31:43 +0100

    Updated

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    string2 =''.join(character for character in my_string
+                     if character.lower() not in {'c', 'C'})
+    return string2
+   
