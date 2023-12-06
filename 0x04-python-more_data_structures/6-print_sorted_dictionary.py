043773c63a6da0f828edfaab00b338ac51a25c82
Salmamjg <salmamejgarii2019@gmail.com>
2023-12-06 21:35:20 +0100

    Updated

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for key in sorted(a_dictionary.keys()):
+    if a_dictionary is None:
+        print("The dictionary is empty or None.")
+        return
+    for key in sorted(a_dictionary):
         print("{}: {}".format(key, a_dictionary[key]))
