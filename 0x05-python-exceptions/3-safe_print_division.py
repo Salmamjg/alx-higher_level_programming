6dce895b9f12118e5856f30f2f1b8186a5311c6e
Salmamjg <salmamejgarii2019@gmail.com>
2023-12-18 17:02:46 +0100

    Updated

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        c = a / b
+    except ZeroDivisionError:
+        print("Error: Division by zero is not allowed.")
+    finally:
+        print("Inside result: {}".format(c))
+        return c
