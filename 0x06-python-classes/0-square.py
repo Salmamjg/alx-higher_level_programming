9cdef53d9d9f0225676fc356585a49935f210f1e
Salmamjg <salmamejgarii2019@gmail.com>
2023-12-29 12:01:33 +0100

    Updated

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,8 +3,5 @@
 """Define a class Square."""
 
 class Square:
-    """ Represents a square.
-
-    This class is currently empty and can be used as a template
-    for defining a square with attributes and methods."""
+    """ Represents a square."""
     pass
