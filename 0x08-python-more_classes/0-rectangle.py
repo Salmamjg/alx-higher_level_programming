4f99db55b9c4ddb0b0019f30597da15bdde15d0f
Salmamjg <salmamejgarii2019@gmail.com>
2024-01-06 18:45:16 +0100

    Updated

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,6 @@
-#!usr/bin/python3
-# 0-rectangle
-"""Define an empty Rectangle class."""
+#!/usr/bin/python3
+# 0-rectangle.py
+"""Defines an empty Rectangle class using the pass keyword"""
 
 
 class Rectangle:
