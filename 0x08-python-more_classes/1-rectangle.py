a1ad9a2f2f557b4d6351d423d9d6f471864a637e
Salmamjg <salmamejgarii2019@gmail.com>
2024-01-07 17:54:21 +0100

    Updated

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,12 +7,6 @@ class Rectangle:
          Args:
         width (int): width
         height (int): height
-    Functions:
-        __init__(self, width, height)
-        width(self)
-        width(self, value)
-        height(self)
-        height(self, value)
     """
 
     def __init__(self, width=0, height=0):
