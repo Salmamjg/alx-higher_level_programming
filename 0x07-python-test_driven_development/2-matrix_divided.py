658174d31d8dc1279ccd78e85f6dfdb2cc9dcf4d
Salmamjg <salmamejgarii2019@gmail.com>
2024-01-25 15:15:31 +0100

    Updated

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -42,4 +42,10 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    return [[round(i / div, 2) for i in l] for l in matrix]
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for num in row:
+            new_row.append(round(num / div, 2))
+        new_matrix.append(new_row)
+    return new_matrix
