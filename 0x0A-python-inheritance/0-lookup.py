2606349e8bc89e57fdb4637f2af36566e8f268a9
Salmamjg <salmamejgarii2019@gmail.com>
2024-02-24 01:51:42 +0100

    Updated

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,7 +6,4 @@ Contains the lookup function
 
 def lookup(obj):
     """returns a list of available attributes and methods of an object"""
-    attributes_and_methods = dir(obj)
-    filtered_attributes_and_methods = [item for item in attributes_and_methods if not item.startswith('_')]
-
-    return filtered_attributes_and_methods
+    return dir(obj)
