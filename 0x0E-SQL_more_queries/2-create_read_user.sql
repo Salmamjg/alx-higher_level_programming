393541d976e9db714421c316058a11863abf10f0
Salmamjg <salmamejgarii2019@gmail.com>
2024-03-05 00:29:19 +0100

    Updated

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,3 @@
+-- script that creates the MySQL server user user_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
