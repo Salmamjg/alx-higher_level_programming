c573af001ae61711cd9ed33df5e230a21b22ef45
Salmamjg <salmamejgarii2019@gmail.com>
2024-03-05 18:20:39 +0100

    update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,7 @@
--- script that creates the MySQL server user user_0d_2;
+-- script that creates the database hbtn_0d_2 and the user user_0d_2
+-- script to create a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- script to create a user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+-- script to grant privileges to user_0d_2
+GRANT ALL PRIVILEGES ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
