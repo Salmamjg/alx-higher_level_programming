82f3718c5e8da89f53223f4514a8de5ff071efa2
Salmamjg <salmamejgarii2019@gmail.com>
2024-03-13 23:40:21 +0000

    Updated

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+
+const args = process.argv.slice(2).map(Number);
+if (args.length === 0 || args.length === 1) {
+console.log(0);
+} else {
+const sortedArgs = args.sort((a, b) => b - a);
+const secondLargest = sortedArgs[1];
+console.log(secondLargest);
+}
