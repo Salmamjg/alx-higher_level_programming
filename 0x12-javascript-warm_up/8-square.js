19e3093325448f6e89aabcc0b639fd7a196a1d06
Salmamjg <salmamejgarii2019@gmail.com>
2024-03-13 16:23:27 +0000

    update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
-const size = Math.floor(Number(process.argv[2]));
-if (isNaN(size)) {
+const y = Math.floor(Number(process.argv[2]));
+if (isNaN(y)) {
     console.log('Missing size');
   } else {
     for (let i = 0; i < y; i++) {
         let row = '';
-        for (let j = 0; j < y; j++) row += 'X';
+        for (let j = 0; j < y; j++) {
+            row += 'X';
+        }
         console.log(row);
     }
   }
