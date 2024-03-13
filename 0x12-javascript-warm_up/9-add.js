14dc45abfa4b031772af96e7e430f2d60d5b6d42
Salmamjg <salmamejgarii2019@gmail.com>
2024-03-13 18:37:58 +0000

    update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-function add(a, b){
-return a + b;
+function add (a, b) {
+    const c = a + b;
+    console.log(c);
 }
-const a = Number(process.argv[2]);
-const b = Number(process.argv[3]);
-console.log(add(a,b));
+add(Number(process.argv[2]), Number(process.argv[3]));
