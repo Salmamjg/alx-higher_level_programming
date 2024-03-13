947802d20eb194856c00535a3ae573511a3dffcf
Salmamjg <salmamejgarii2019@gmail.com>
2024-03-13 18:06:30 +0000

    update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,12 +1,5 @@
-#!/usr/bin/node
+
 function add(a, b){
     return a + b;
 }
-const a = parseInt(Number(process.argv[2]));
-const b = parseInt(Number(process.argv[3]));
-if(isNaN(a) || isNaN(b)){
-    console.log('NaN');
-} else{
-    const result = add(a, b);
-    console.log(`${result}`);
-}
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
