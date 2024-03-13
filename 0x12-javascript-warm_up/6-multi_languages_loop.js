d187ece613d4b9257f1430ef455c06b1ac2a0a7a
Salmamjg <salmamejgarii2019@gmail.com>
2024-03-13 15:05:36 +0000

    update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (const phrase of ['C is fun', 'Python is cool', 'JavaScript is amazing']){
-    console.log(phrase);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']){
+    console.log(line);
 }
