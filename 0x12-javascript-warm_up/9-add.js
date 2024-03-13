2d9ff58d1ca984cd831739fbd43edec4d2de0089
Salmamjg <salmamejgarii2019@gmail.com>
2024-03-13 18:19:59 +0000

    update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,15 +2,14 @@
 
 function add(a, b) {
     return a + b;
-  }
-  
-  const arg1 = parseInt(process.argv[2]);
-  const arg2 = parseInt(process.argv[3]);
-  
-  if (isNaN(arg1) || isNaN(arg2)) {
+}
+
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
+
+if (isNaN(arg1) || isNaN(arg2)) {
     console.log('Please provide two valid integers as arguments.');
-  } else {
+} else {
     const result = add(arg1, arg2);
     console.log(`${arg1} + ${arg2} = ${result}`);
-  }
-  
+}
