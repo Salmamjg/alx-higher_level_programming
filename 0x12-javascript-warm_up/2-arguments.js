7dfe6d499352d35f355ebd51274f514fccdf9e9e
Salmamjg <salmamejgarii2019@gmail.com>
2024-03-13 01:07:54 +0000

    update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
 const argsCount = process.argv.length - 2;
-if (argsCount === 0) {
-  console.log("No argument");
-} else if (argsCount === 1) {
-  console.log("Argument found");
-} else {
-  console.log("Arguments found");
-}
+console.log(count === 0 ? 'No argument' : count === 1 ? 'Argument found' : 'Arguments found');
